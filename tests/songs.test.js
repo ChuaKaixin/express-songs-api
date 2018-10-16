ac31dec88ddb811ec310023d173feafadf8ef2da
ChuaKaixin <chuakaixin.working@gmail.com>
2018-10-16 13:44:24 +0800

    added testing

--- tests/songs.test.js
+++ tests/songs.test.js
@@ -6,27 +6,75 @@ const request = require("supertest");
 describe("routes/songs", () => {
 
   it("POST /songs should return a new song object", () => {
-   
+    //expected return value
+    expected = {name: "Song name", artist: "Song Artist"}
+    //trigger post request
+    return request(app).post("/songs").send(expected)
+    .then(response => {
 
+      //check response returned
+      expect(response.status).toEqual(201);
+      //check response body = expected
+      expect(response.body).toMatchObject(expected);
+
+    })
   });
   
   it("GET /songs should return a non empty array", () => {
-   
+       //expected return value
+      expected = {name: "Song name", artist: "Song Artist"}
+      //trigger post request
+      return request(app).get("/songs")
+      .then(response => {
+        //check response returned
+        expect(response.status).toEqual(200);
+        //check response body = expected
+        expect(response.body).toMatchObject([expected]);
+      
+    })
 
   });
   
   it("PUT /songs should return the updated song", () => {
-    
+     //expected return value
+     expected = {name: "New Song name", artist: "New Song Artist"}
+     //trigger post request
+     return request(app).put("/songs/1").send(expected)
+     .then(response => {
+       //check response returned
+       expect(response.status).toEqual(200);
+       //check response body = expected
+       expect(response.body).toMatchObject(expected);
+ 
+     })
 
   });
 
   it("DELETE /songs/:id should return the deleted song", () => {
-    
+    //expected return value
+    expected = {name: "New Song name", artist: "New Song Artist"}
+    //trigger post request
+    return request(app).delete("/songs/1").send(expected)
+    .then(response => {
+
+      //check response returned
+      expect(response.status).toEqual(200);
+      //check response body = expected
+      expect(response.body).toMatchObject(expected);
+
+    })
 
   });
   
   it("GET /songs should return an empty array", () => {
-    
+     //trigger post request
+     return request(app).get("/songs")
+     .then(response => {
+       //check response returned
+       expect(response.status).toEqual(200);
+       //check response body = expected
+       expect(response.body).toMatchObject([]);
+   })
     
   });
 
